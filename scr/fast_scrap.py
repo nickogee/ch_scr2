5dd1db08bd4db17b78dc5f0b61939e3edef9a37c
Nikolay Ognev <ognev.n@ryadom.kz>
2023-09-29 17:35:13 +0600

    fast scrap run on sunday

--- scr/fast_scrap.py
+++ scr/fast_scrap.py
@@ -2,24 +2,32 @@ import scr.arbuz_api_scraper as abz
 import scr.glovo_api_scraper as glv
 import scr.magnum_api_scraper as mgm
 import scr.airba_api_scraper as air
+import datetime
+
+
+def run_fast_scrapers():
+
+    if datetime.datetime.today().weekday() == 7:
+        try:
+            abz.fast_category_scraper()
+        except Exception as ex:
+            print(ex)
+        
+        try:
+            glv.fast_category_scraper()
+        except Exception as ex:
+            print(ex)
+
+        try:
+            mgm.fast_category_scraper()
+        except Exception as ex:
+            print(ex)
+            
+        try:
+            air.fast_category_scraper()
+        except Exception as ex:
+            print(ex)
+
 
 if __name__ == '__main__':
-    try:
-        abz.fast_category_scraper()
-    except Exception as ex:
-        print(ex)
-    
-    try:
-        glv.fast_category_scraper()
-    except Exception as ex:
-        print(ex)
-
-    try:
-        mgm.fast_category_scraper()
-    except Exception as ex:
-        print(ex)
-           
-    try:
-        air.fast_category_scraper()
-    except Exception as ex:
-        print(ex)
+    run_fast_scrapers()
